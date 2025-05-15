03399edc1f02cf112dc79b8f05235b1f46ce2ec4
Gonçalo <goncalo.ferreira@student.uva.nl>
2025-05-15 20:27:25 +0200

    Updating the installing script.

--- export-metrics-service/export-metrics-pod/export-metrics.sh
+++ export-metrics-service/export-metrics-pod/export-metrics.sh
@@ -1,2 +1,3 @@
+pip install -r requirements.txt
 mkdir -p /home/jovyan/export-metrics
 python export_metrics.py
--- scaphandre-prometheus-ownpod/install-scaphandre-prometheus.sh
+++ scaphandre-prometheus-ownpod/install-scaphandre-prometheus.sh
@@ -9,15 +9,22 @@ rustup install 1.65.0
 rustup override set 1.65.0
 
 # Install Scaphandre
+cd ~
 git clone https://github.com/hubblo-org/scaphandre.git
 cd scaphandre
 cargo build --release
+sudo mv ./target/release/scaphandre /usr/local/bin/
+cd ~ && rm -rf scaphandre
+
+# Run Scaphandre server in the background, with metrics compatible with Prometheus
+nohup scaphandre prometheus --address=0.0.0.0 --port=8081 --containers > scaphandre.log 2>&1 &
 
 # Install Prometheus
 cd ~
 wget https://github.com/prometheus/prometheus/releases/download/v2.52.0/prometheus-2.52.0.linux-amd64.tar.gz
 tar xzf prometheus-2.52.0.linux-amd64.tar.gz
-cd prometheus-2.52.0.linux-amd64
+mv prometheus-2.52.0.linux-amd64 prometheus-unzipped
+rm -rf prometheus-2.52.0.linux-amd64.tar.gz
 
 # Start Prometheus server
 PROMETHEUS_CONFIG=$(cat <<EOF
@@ -30,5 +37,6 @@ scrape_configs:
       - targets: ['localhost:8081']
 EOF
 )
+
 echo "$PROMETHEUS_CONFIG" > prometheus.yml
-./prometheus --config.file=prometheus.yml --web.listen-address=0.0.0.0:9090
+nohup ~/prometheus-unzippedprometheus --config.file=prometheus.yml --web.listen-address=0.0.0.0:9090 > prometheus.log 2>&1 &
