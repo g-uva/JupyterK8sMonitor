60c412b15512b1e72ba7ae1a1f5f3ac013f536a3
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-05-19 15:00:05 +0200

    Fix of the script.

--- icenet-notebook-downloader/download-icenet-notebook.sh
+++ icenet-notebook-downloader/download-icenet-notebook.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y libhdf5-dev libnetcdf-dev
-
-pip install -r /home/jovyan/egi-ice-net-example/requirements.txt
+# TODO: download the configuration file from the server.
