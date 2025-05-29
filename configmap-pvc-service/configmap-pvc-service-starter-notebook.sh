79020f797a1e5db7a4bd6f183d0fb63aa400b6f0
Gonçalo <g.j.teixeiradepinhoferreira@uva.nl>
2025-05-29 18:39:43 +0200

    Separating the notebook into the stress one.

--- configmap-pvc-service/configmap-pvc-service-starter-notebook.sh
+++ configmap-pvc-service/configmap-pvc-service-starter-notebook.sh
@@ -1,3 +1,8 @@
 kubectl create configmap starter-notebook \
   --from-file=GD_EcoJupyter_Tutorial.ipynb=/home/goncalo/jhub-helm-config/tutorial-notebook/GD_EcoJupyter_Tutorial.ipynb \
   -n jhub
+
+kubectl create configmap stress-notebook \
+  --from-file=GD_EcoJupyter_StressTest.ipynb=/home/goncalo/jhub-helm-config/tutorial-notebook/GD_EcoJupyter_StressTest.ipynb \
+  -n jhub
+
